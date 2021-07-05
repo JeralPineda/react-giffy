8845aad33fd0ab1c2e24da8af71c2e62dc49a671
Jeral <jeral.cris@gmail.com>
2021-07-04 20:38:15 -0600

    haciendo uso del context con el provider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import { Link, Route } from 'wouter';
 
-import './App.css';
 import { Detail } from './pages/Detail';
 import { Home } from './pages/Home';
 import { SearchResults } from './pages/SearchResults';
+import StaticContext from './context/StaticContext';
+import './App.css';
 
 function App() {
    return (
-      <div className='App'>
-         <section className='App-content'>
-            <Link className='App-logo' alt='Giffy logo' to='/'>
-               <img src='./favicon.ico' alt='' />
-            </Link>
-            <Route path='/' component={Home} />
-            <Route path='/search/:keyword' component={SearchResults} />
-            <Route path='/gif/:id' component={Detail} />
-         </section>
-      </div>
+      <StaticContext.Provider value={{ name: 'Jeral', Conectado: true }}>
+         <div className='App'>
+            <section className='App-content'>
+               <Link className='App-logo' alt='Giffy logo' to='/'>
+                  <img src='./favicon.ico' alt='' />
+               </Link>
+
+               <Route path='/' component={Home} />
+
+               <Route path='/search/:keyword' component={SearchResults} />
+
+               <Route path='/gif/:id' component={Detail} />
+            </section>
+         </div>
+      </StaticContext.Provider>
    );
 }
 
