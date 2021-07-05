fc6f68697d716fb81066a83a064b54215fa07d8e
Jeral <jeral.cris@gmail.com>
2021-07-04 18:11:37 -0600

    Limpiando codigo comentado (useGifs)

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -9,19 +9,5 @@ export const SearchResults = ({ params }) => {
 
    const { loading, gifs } = useGifs({ keyword });
 
-   /*
-   const [loading, setLoading] = useState(false);
-
-   const [gifs, setGifs] = useState([]);
-
-   useEffect(() => {
-      setLoading(true);
-      getGif({ keyword }).then((gifs) => {
-         setGifs(gifs);
-         setLoading(false);
-      });
-   }, [keyword]);
-   */
-
    return <>{loading ? <Loader /> : <ListOfGifs gifs={gifs} />}</>;
 };
