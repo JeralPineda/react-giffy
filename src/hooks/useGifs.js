eab42eb21350cbe64c551ff410980d28f5e6140e
Jeral <jeral.cris@gmail.com>
2021-07-04 19:51:37 -0600

    localstorage para cargar gif por defecto segun la ultima busqueda

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,16 +1,23 @@
 import { useEffect, useState } from 'react';
 import { getGif } from '../services/getGifs';
 
-export const useGifs = ({ keyword }) => {
+export const useGifs = ({ keyword } = { keyword: null }) => {
    const [loading, setLoading] = useState(false);
 
    const [gifs, setGifs] = useState([]);
 
    useEffect(() => {
       setLoading(true);
-      getGif({ keyword }).then((gifs) => {
+
+      //   Recuperamos la keyword del localstorage
+      const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random';
+
+      getGif({ keyword: keywordToUse }).then((gifs) => {
          setGifs(gifs);
          setLoading(false);
+
+         //  Guardamos la keyword en el localstorage
+         localStorage.setItem('lastKeyword', keyword);
       });
    }, [keyword]);
 
