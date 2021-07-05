c1fc75262f1f15710aaa6e80c6d35daca814a595
Jeral <jeral.cris@gmail.com>
2021-07-04 21:47:27 -0600

    cambiando el rango de la peticion de las gifs de 10 a 20

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,7 +1,7 @@
 const apiKey = 'yfxK1j1iSVjkUj0lGKX2RKL8Oqyu0svM';
 
 export const getGif = async ({ keyword = 'One Piece' } = {}) => {
-   const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`;
+   const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=20&offset=0&rating=g&lang=en`;
 
    const resp = await fetch(apiUrl);
 
