edfb69337e2eb8428b420ab5c5521218ade116a6
Jeral <jeral.cris@gmail.com>
2021-07-05 21:27:42 -0600

    Haciendo un cambio en el codigo de la peticion a la API giphy

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,22 +1,43 @@
 const apiKey = 'yfxK1j1iSVjkUj0lGKX2RKL8Oqyu0svM';
 
-export const getGif = async ({ keyword = 'One Piece' } = {}) => {
+// export const getGif = async ({ keyword = 'One Piece' } = {}) => {
+//    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=20&offset=0&rating=g&lang=en`;
+
+//    const resp = await fetch(apiUrl);
+
+//    const { data } = await resp.json();
+
+//    const gifs = data.map((img) => {
+//       const { title, id } = img;
+//       const { url } = img.images.downsized_medium;
+
+//       return {
+//          title,
+//          id,
+//          url,
+//       };
+//    });
+
+//    return gifs;
+// };
+
+const fromApiResponseToGif = (apiResponse) => {
+   const { data = [] } = apiResponse;
+   if (Array.isArray(data)) {
+      const gifs = data.map((image) => {
+         const { images, title, id } = image;
+         const { url } = images.downsized_medium;
+         return { title, id, url };
+      });
+      return gifs;
+   }
+   return [];
+};
+
+export const getGif = ({ limit = 20, keyword = 'morty' } = {}) => {
    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=20&offset=0&rating=g&lang=en`;
 
-   const resp = await fetch(apiUrl);
-
-   const { data } = await resp.json();
-
-   const gifs = data.map((img) => {
-      const { title, id } = img;
-      const { url } = img.images.downsized_medium;
-
-      return {
-         title,
-         id,
-         url,
-      };
-   });
-
-   return gifs;
+   return fetch(apiUrl)
+      .then((res) => res.json())
+      .then(fromApiResponseToGif);
 };
