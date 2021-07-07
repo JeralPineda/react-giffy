0e194a2a278542044b6670064ade295d8c06858d
Jeral <jeral.cris@gmail.com>
2021-07-06 22:50:04 -0600

    useRef para obtener la referencai al elemento y metodo diconnect para limpiar el evento cuando se deje de utilizar

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { getTrendingTerms } from 'services/getTrendingTermsService';
 import Category from '../Category';
@@ -16,20 +16,26 @@ const TrendingSearches = () => {
 export const LazyTrending = () => {
    const [show, setShow] = useState(false);
 
+   const elementRef = useRef();
+
    useEffect(() => {
-      const onChange = (entries) => {
+      const onChange = (entries, observer) => {
          const el = entries[0];
 
          if (el.isIntersecting) {
             setShow(true);
+            observer.disconnect(); //una ves que se hace la interseccion se desconecta
          }
       };
+
       const observer = new IntersectionObserver(onChange, {
          rootMargin: '100px',
       });
 
-      observer.observe(document.getElementById('lazyTrending'));
-   }, []);
+      observer.observe(elementRef.current);
 
-   return <div id='lazyTrending'>{show ? <TrendingSearches /> : null}</div>;
+      return () => observer.disconnect(); //cuando el componente se deje de utilizar no se muestre y limpie el elemento cuando no esta disponible
+   });
+
+   return <div ref={elementRef}>{show ? <TrendingSearches /> : null}</div>;
 };
