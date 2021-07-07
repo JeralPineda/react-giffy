d753c4caef1a67579c43c231cf454a7aff94d3e8
Jeral <jeral.cris@gmail.com>
2021-07-06 23:30:29 -0600

    moviendo el lazy load a un hook y dejando el codigo de los trending mas limpio

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { getTrendingTerms } from 'services/getTrendingTermsService';
-import Category from '../Category';
+import Category from 'components/Category';
+import { useNearScreen } from 'hooks/useNearScreen';
 
 const TrendingSearches = () => {
    const [trends, setTrends] = useState([]);
@@ -14,32 +15,7 @@ const TrendingSearches = () => {
 };
 
 export const LazyTrending = () => {
-   const [show, setShow] = useState(false);
+   const { isNearScreen, fromRef } = useNearScreen({ distance: '200px' });
 
-   const elementRef = useRef();
-
-   useEffect(() => {
-      let observer = '';
-
-      const onChange = (entries, observer) => {
-         const el = entries[0];
-
-         if (el.isIntersecting) {
-            setShow(true);
-            observer.disconnect(); //una ves que se hace la interseccion se desconecta
-         }
-      };
-
-      Promise.resolve(typeof IntersectionObserver !== 'undefined' ? IntersectionObserver : import('intersection-observer')).then(() => {
-         const observer = new IntersectionObserver(onChange, {
-            rootMargin: '100px',
-         });
-
-         observer.observe(elementRef.current);
-      });
-
-      return () => observer && observer.disconnect(); //cuando el componente se deje de utilizar no se muestre y limpie el elemento cuando no esta disponible
-   });
-
-   return <div ref={elementRef}>{show ? <TrendingSearches /> : null}</div>;
+   return <div ref={fromRef}>{isNearScreen ? <TrendingSearches /> : null}</div>;
 };
