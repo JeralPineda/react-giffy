3a42f62e6cfb34a34005c53ab95677f93328e45b
Jeral <jeral.cris@gmail.com>
2021-07-07 22:42:29 -0600

    2 nuevos useState y useEffect para la paginacion de la app

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -2,15 +2,19 @@ import { useContext, useEffect, useState } from 'react';
 import GifsContext from 'context/GifsContext';
 import { getGif } from 'services/getGifs';
 
+const initPage = 0;
+
 export const useGifs = ({ keyword } = { keyword: null }) => {
    const [loading, setLoading] = useState(false);
+   const [loadingNextPge, setLoadingNextPge] = useState(false);
+   const [page, setPage] = useState(initPage);
    const { gifs, setGifs } = useContext(GifsContext);
+
+   //   Recuperamos la keyword del localstorage
+   const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random';
 
    useEffect(() => {
       setLoading(true);
-
-      //   Recuperamos la keyword del localstorage
-      const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random';
 
       getGif({ keyword: keywordToUse }).then((gifs) => {
          setGifs(gifs);
@@ -19,10 +23,25 @@ export const useGifs = ({ keyword } = { keyword: null }) => {
          //  Guardamos la keyword en el localstorage
          localStorage.setItem('lastKeyword', keyword);
       });
-   }, [keyword, setGifs]);
+   }, [keyword, keywordToUse, setGifs]);
+
+   useEffect(() => {
+      if (page === initPage) return;
+
+      setLoadingNextPge(true); //antes de hacer la llamada se pone a cargar
+
+      getGif({ keyword: keywordToUse, page }).then((nextGifs) => {
+         //   obtenemos el estado anterior
+         setGifs((prevGifs) => prevGifs.concat(nextGifs));
+
+         setLoadingNextPge(false);
+      });
+   }, [keywordToUse, page, setGifs]);
 
    return {
       loading,
+      loadingNextPge,
       gifs,
+      setPage,
    };
 };
